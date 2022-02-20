0381312c997bd811657efbe4c9ae3943efacb212
cwins <cwinston7g@gmail.com>
2022-02-20 12:50:35 -0500

    2

--- venv/numPyStuff.py
+++ venv/numPyStuff.py
@@ -9,4 +9,19 @@ import numpy as np
 
 myArray = np.linspace(0,10,101)
 
-print(myArray)
+#print(myArray)
+
+
+# TASK: Use numpy to check how many rolls were greater than 2. For example if dice_rolls=[1,2,3] then the answer is 1.
+# NOTE: Many different ways to do this! Your final answer should be an integer.
+# MAKE SURE TO READ THE FULL INSTRUCTIONS ABOVE CAREFULLY, AS THE EVALUATION SCRIPT IS VERY STRICT.
+# Link to Solution: https://gist.github.com/Pierian-Data/ea3121efac5dd3338c280ff10068f9c8
+
+
+dice_rolls = np.array([3, 1, 5, 2, 5, 1, 1, 5, 1, 4, 2, 1, 4, 5, 3, 4, 5, 2, 4, 2, 6, 6, 3, 6, 2, 3, 5, 6, 5])
+
+bool_arr = dice_rolls > 2
+#print(dice_rolls[bool_arr])
+
+total_rolls_over_two = len(dice_rolls[bool_arr])
+print(total_rolls_over_two)

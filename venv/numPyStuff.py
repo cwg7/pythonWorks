bd3d428bd5a0701c31c6846473b27e6311d6fd4a
cwins <cwinston7g@gmail.com>
2022-02-20 11:50:10 -0500

    1

---
+++ venv/numPyStuff.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# TASK: Create a numpy array called myarray which consists of 101 evenly linearly spaced points between 0 and 10.
+# MAKE SURE TO READ THE FULL INSTRUCTIONS ABOVE CAREFULLY, AS THE EVALUATION SCRIPT IS VERY STRICT.
+# Link to Solution: https://gist.github.com/Pierian-Data/ea9c4d2fc6c98ac74af18134cd924867
+# import ?
+# myarray = ?
+
+
+myArray = np.linspace(0,10,101)
+
+print(myArray)

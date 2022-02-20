7482799189a9c820f9c6bb37c79ef0e553b1104a
cwins <cwinston7g@gmail.com>
2022-02-20 13:55:15 -0500

    3

--- venv/numPyStuff.py
+++ venv/numPyStuff.py
@@ -24,4 +24,17 @@ bool_arr = dice_rolls > 2
 #print(dice_rolls[bool_arr])
 
 total_rolls_over_two = len(dice_rolls[bool_arr])
-print(total_rolls_over_two)
+#print(total_rolls_over_two)
+
+
+# TASK: Use numpy to check the total remaining in the account after the series of transactions.
+# NOTE: Many different ways to do this!
+# MAKE SURE TO READ THE FULL INSTRUCTIONS ABOVE CAREFULLY, AS THE EVALUATION SCRIPT IS VERY STRICT.
+# Link to Solution: https://gist.github.com/Pierian-Data/225a449484e12e0535fbbac2231b426b
+
+import numpy as np
+account_transactions = np.array([100,-200,300,-400,100,100,-230,450,500,2000])
+
+# SOLUTION
+account_total = np.sum(account_transactions)
+#print(account_total)

bc5207051789c276c3d9f5ebc59737a0c64b8946
cwins <cwinston7g@gmail.com>
2022-02-21 13:25:34 -0500

    dataframe example

--- venv/pandas1.py
+++ venv/pandas1.py
@@ -18,8 +18,20 @@ mySer = pd.Series(data = myData, index = myIndex)
 #  Link to Solution: https://gist.github.com/Pierian-Data/3d7f7cb3528f015d9584d04a7168b97f
 
 expenses = pd.Series({'Andrew':200,'Bob':150,'Claire':450})
+print(expenses)
 
 # bob_expense = # Use pandas, don't just manually write in the number here.
 
 bob_expense = expenses['Bob']
-print(bob_expense)
+
+#print(bob_expense)
+
+# Set Dataframe as csv file (example)
+
+df = pd.read_csv('C:\\Users\\cwins\\Downloads\\UNZIP_FOR_NOTEBOOKS_FINAL (1)\\03-Pandas\\tips.csv')
+#print(df)
+
+# Set 'Payment ID' Column title as Dataframe Index
+
+df = df.set_index('Payment ID')
+#print(df)
